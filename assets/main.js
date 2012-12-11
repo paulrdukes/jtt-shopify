5e22a4f11c8a2b3348467744371665877cb31997
Paul Dukes <paul.r.dukes@gmail.com>
2012-12-11 04:06:24 -0500

    styled cart add js plugins and main

--- assets/main.js
+++ assets/main.js
@@ -1,16 +1,4 @@
 $(document).ready(function(){
-	var $window = $(window);
 
-	function checkWidth(){
-		var curWidth = $window.width();
 
-		if(curWidth < 400 ){
-			$('aside').addClass('hidden');
-		} else {
-			$('aside').removeClass('hidden');
-		}
-
-	}
-	checkWidth();
-	$(window).resize(checkWidth);
 });
